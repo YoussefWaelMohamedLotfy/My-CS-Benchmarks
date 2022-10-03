9076f8207fc8aac5405405316c4f345c09bf8d39
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-10-03 23:35:37 +0200

    Added Benchmarks for DbContextBenchmarks

--- src/EFCore/Benchmarks/DbContextBenchmarks.cs
+++ src/EFCore/Benchmarks/DbContextBenchmarks.cs
@@ -1,9 +1,50 @@
 ﻿using BenchmarkDotNet.Attributes;
+using EFCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace EFCore.Benchmarks;
 
 [MemoryDiagnoser]
 public class DbContextBenchmarks
 {
+    private DbContextOptions<AppDbContext> options = new DbContextOptionsBuilder<AppDbContext>()
+        .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EF-Benchmarks;Integrated Security=True;Connect Timeout=30")
+        .Options;
 
+    private PooledDbContextFactory<AppDbContext> _dbContextPoolingFactory;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _dbContextPoolingFactory = new PooledDbContextFactory<AppDbContext>(options);
+    }
+
+    [Benchmark(Baseline = true)]
+    public List<Person> GetAll_WithoutContextPooling()
+    {
+        using var context = new AppDbContext(options);
+        return context.People.ToList();
+    }
+
+    [Benchmark]
+    public List<Person> GetAll_WithoutContextPooling_AsNoTracking()
+    {
+        using var context = new AppDbContext(options);
+        return context.People.AsNoTracking().ToList();
+    }
+
+    [Benchmark]
+    public List<Person> GetAll_WithContextPooling()
+    {
+        using var context = _dbContextPoolingFactory.CreateDbContext();
+        return context.People.ToList();
+    }
+
+    [Benchmark]
+    public List<Person> GetAll_WithContextPooling_AsNoTracking()
+    {
+        using var context = _dbContextPoolingFactory.CreateDbContext();
+        return context.People.AsNoTracking().ToList();
+    }
 }
--- src/EFCore/Data/AppDbContext.cs
+++ src/EFCore/Data/AppDbContext.cs
@@ -5,20 +5,11 @@ namespace EFCore.Data;
 
 public sealed class AppDbContext : DbContext
 {
-    public AppDbContext()
-    {
-    }
-
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
 
     public DbSet<Person>? People { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EF-Benchmarks;Integrated Security=True;Connect Timeout=30");
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
